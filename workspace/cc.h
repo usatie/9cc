cc38336b98ed075a3c00a7cae51bf7e31e644ac8
Shun Usami <usatie@gmail.com>
2019-04-28 20:48:35 +0900

    :recycle: Refactor by splitting files into pieces.
    * Add cc.h
    * Rename 9cc.c to main.c
    * Add vector.c
    * Modify other parts to fit to the changes above

---
+++ workspace/cc.h
@@ -0,0 +1,20 @@
+#ifndef CC_CC_H
+#define CC_CC_H
+
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// vector.c
+typedef struct {
+	void **data;
+	int capacity;
+	int len;
+} Vector;
+
+Vector *new_vector();
+void vec_push(Vector *vec, void *elem);
+
+#endif
--- workspace/main.c
+++ workspace/main.c
@@ -1,32 +1,4 @@
-#include <ctype.h>
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-// Vector
-typedef struct {
-	void **data;
-	int capacity;
-	int len;
-} Vector;
-
-Vector *new_vector() {
-	Vector *vec = malloc(sizeof(Vector));
-	vec->data = malloc(sizeof(void *) * 16);
-	vec-> capacity = 16;
-	vec->len = 0;
-	return vec;
-}
-
-void vec_push(Vector *vec, void *elem) {
-	if (vec->capacity == vec->len) {
-		vec->capacity *= 2;
-		vec->data = realloc(vec->data, sizeof(void *) * vec->capacity);
-	}
-	vec->data[vec->len++] = elem;
-}
-
+#include "cc.h"
 
 // Token
 /// Token type
