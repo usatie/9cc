68c80f5b59573f2beb23024c1cab286df02c949a
Shun Usami <usatie@gmail.com>
2019-05-02 17:50:55 +0900

    :recycle: Refactor codegen.c with emit() and p()

--- workspace/codegen.c
+++ workspace/codegen.c
@@ -2,56 +2,71 @@
 
 int nlabel = 0;
 
+static void p(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vprintf(fmt, ap);
+  printf("\n");
+}
+
+static void emit(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  printf("\t");
+  vprintf(fmt, ap);
+  printf("\n");
+}
+
 void gen_lval(Node *node) {
   if (node->ty != ND_IDENT)
     error("The left hand of assignment is not an identifier.");
 
-  printf("	mov rax, rbp\n");
-  printf("	sub rax, %d\n", node->offset);
-  printf("	push rax\n");
+  emit("mov rax, rbp");
+  emit("sub rax, %d", node->offset);
+  emit("push rax");
 }
 
 /// Generate assembly by Node
 void gen(Node *node) {
   if (node->ty == ND_RETURN) {
     gen(node->lhs);
-    printf("	pop rax\n");
-    printf("	mov rsp, rbp\n");
-    printf("	pop rbp\n");
-    printf("	ret\n");
+    emit("pop rax");
+    emit("mov rsp, rbp");
+    emit("pop rbp");
+    emit("ret");
     return;
   }
   if (node->ty == ND_IF) {
     gen(node->cond);
-    printf("	pop rax\n");
-    printf("	cmp rax, 0\n");
+    emit("pop rax");
+    emit("cmp rax, 0");
     if (node->els) {
       // ifelse
-      printf("	je .Lelse%d\n", nlabel);
+      emit("je .Lelse%d", nlabel);
       gen(node->then);
-      printf("	jmp .Lend%d\n", nlabel);
-      printf(".Lelse%d:\n", nlabel);
+      emit("jmp .Lend%d", nlabel);
+      p(".Lelse%d:", nlabel);
       gen(node->els);
-      printf(".Lend%d:\n", nlabel);
+      p(".Lend%d:", nlabel);
     } else {
       // if
-      printf("	je .Lend%d\n", nlabel);
+      emit("je .Lend%d", nlabel);
       gen(node->then);
-      printf(".Lend%d:\n", nlabel);
+      p(".Lend%d:", nlabel);
     }
     nlabel++;
     return;
   }
   if (node->ty == ND_NUM) {
-    printf("	push %d\n", node->val);
+    emit("push %d", node->val);
     return;
   }
 
   if (node->ty == ND_IDENT) {
     gen_lval(node);
-    printf("	pop rax\n");
-    printf("	mov rax, [rax]\n");
-    printf("	push rax\n");
+    emit("pop rax");
+    emit("mov rax, [rax]");
+    emit("push rax");
     return;
   }
 
@@ -59,54 +74,54 @@ void gen(Node *node) {
     gen_lval(node->lhs);
     gen(node->rhs);
 
-    printf("	pop rdi\n");
-    printf("	pop rax\n");
-    printf("	mov [rax], rdi\n");
-    printf("	push rdi\n");
+    emit("pop rdi");
+    emit("pop rax");
+    emit("mov [rax], rdi");
+    emit("push rdi");
     return;
   }
 
   gen(node->lhs);
   gen(node->rhs);
 
-  printf("	pop rdi\n");
-  printf("	pop rax\n");
+  emit("pop rdi");
+  emit("pop rax");
 
   switch (node->ty) {
   case '+':
-    printf("	add rax, rdi\n");
+    emit("add rax, rdi");
     break;
   case '-':
-    printf("	sub rax, rdi\n");
+    emit("sub rax, rdi");
     break;
   case '*':
-    printf("	mul rdi\n");
+    emit("mul rdi");
     break;
   case '/':
-    printf("	mov rdx, 0\n");
-    printf("	div rdi\n");
+    emit("mov rdx, 0");
+    emit("div rdi");
     break;
   case TK_EQ:
-    printf("	cmp rax, rdi\n");
-    printf("	sete al\n");
-    printf("	movzb rax, al\n");
+    emit("cmp rax, rdi");
+    emit("sete al");
+    emit("movzb rax, al");
     break;
   case TK_NE:
-    printf("	cmp rax, rdi\n");
-    printf("	setne al\n");
-    printf("	movzb rax, al\n");
+    emit("cmp rax, rdi");
+    emit("setne al");
+    emit("movzb rax, al");
     break;
   case '<':
-    printf("	cmp rax, rdi\n");
-    printf("	setl al\n");
-    printf("	movzb rax, al\n");
+    emit("cmp rax, rdi");
+    emit("setl al");
+    emit("movzb rax, al");
     break;
   case TK_LE:
-    printf("	cmp rax, rdi\n");
-    printf("	setle al\n");
-    printf("	movzb rax, al\n");
+    emit("cmp rax, rdi");
+    emit("setle al");
+    emit("movzb rax, al");
     break;
   }
 
-  printf("	push rax\n");
+  emit("push rax");
 }
